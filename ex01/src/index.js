df3acf1022d58024498865ef6a40fb889c14a914
Haris Pasic <harispasic@ymail.com>
2022-04-18 03:12:28 +0200

    Generator Functions

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,19 @@
+let myArr = [];
+// Only change code below this line
+function* multiplication(x){
+    for(var i=0; i<x; i++)
+    {
+        x = x * 2;
+    yield x;
+    myArr.push(x); 
+}
+}
 
+var num = multiplication(3);
+
+console.log(num.next().value);
+console.log(num.next().value);
+console.log(num.next().value);
+
+// Only change code above this line
+module.exports = multiplication;
